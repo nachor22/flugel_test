cf32be4e8d2dbcd90e6d4e3be8343c50f3045e16
Nacho Retamoza <nachor22@gmail.com>
2021-06-14 11:34:04 -0300

    remove output not used

--- 01_s3.tf
+++ 01_s3.tf
@@ -29,7 +29,3 @@ resource "aws_s3_bucket_object" "test2" {
 output "bucket_objects_time" {
   value = time_static.t.rfc3339
 }
-
-output "bucket_domain" {
-  value = aws_s3_bucket.fb.bucket_regional_domain_name
-}
