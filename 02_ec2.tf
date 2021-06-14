e0656802ee27daaed2b0ac8a8f961e0d9b3b10ed
Nacho Retamoza <nachor22@gmail.com>
2021-06-14 10:05:36 -0300

    lb and web, ec2 resources

--- 02_ec2.tf
+++ 02_ec2.tf
@@ -15,26 +15,45 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-data "template_file" "userdata" {
-  template = file("userdata.yaml")
+data "template_file" "ud-lb" {
+  template = file("userdata/userdata_traefik.yaml")
   vars = {
-    s3_domain_name = aws_s3_bucket.fb.bucket_regional_domain_name
+    ec2_domain_name = aws_instance.web01.private_dns
+  }
+}
+
+data "template_file" "ud-web" {
+  template = file("userdata/userdata_web.yaml")
+  vars = {
+    s3_bucket_name = aws_s3_bucket.fb.id
   }
 }
 
 ##resources
-resource "aws_instance" "web" {
+resource "aws_instance" "lb" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  user_data = data.template_file.userdata.rendered
+  user_data = data.template_file.ud-lb.rendered
   key_name = "nachor_key"
   subnet_id = aws_subnet.main.id
-  vpc_security_group_ids = ["${aws_security_group.http-https-allowed.id}"]
+  vpc_security_group_ids = ["${aws_security_group.lb.id}"]
+  #depends on internet gateway
+  #depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_instance" "web01" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  user_data = data.template_file.ud-web.rendered
+  key_name = "nachor_key"
+  subnet_id = aws_subnet.main.id
+  vpc_security_group_ids = ["${aws_security_group.web.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.prof.name}"
   #depends on internet gateway
   #depends_on = [aws_internet_gateway.gw]
 }
 
 ##outputs
-output "ec2_domain" {
-  value = aws_instance.web.public_dns
+output "lb_domain" {
+  value = aws_instance.lb.public_dns
 }
